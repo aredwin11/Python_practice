8fd979d5b3782efbe685d17cb05572fa199fae98
aredwin11 <edwin.a@diggibyte.com>
2025-05-28 19:01:11 +0530

    Python practice on wednesday

---
+++ Type-functions.py
@@ -0,0 +1,23 @@
+# type functions is used to find the datatype of given variables in python
+d=10
+t=type(d)
+print(t)
+
+#class float
+s=12.0
+t=type(s)
+print(t)
+
+#class string
+m="edwin"
+t=type(m)
+print(t)
+
+v="21.4" # anything under the double quotes are considered as string
+t=type(v)
+print(t)
+
+#now we can convert v as float datatype
+t=float(v)
+b=type(t)
+print(b)
---
+++ variables.py
@@ -0,0 +1,6 @@
+seven =7 #valid variable
+print("seven")
+_seven2=9 #valid variable
+print(_seven2)
+# @seven=3 --> invalid variable
+# 1ed="hari" --> invalid variable
