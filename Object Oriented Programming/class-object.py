ef48b466d9875d9e1718535a40aba50bb7062815
aredwin11 <edwin.a@diggibyte.com>
2025-06-03 19:12:41 +0530

    Error Handling Completed

---
+++ Object Oriented Programming/class-object.py
@@ -0,0 +1,17 @@
+class Laptop:
+    ram=""
+    processor=""
+    price=""
+
+
+Hp=Laptop()
+Dell=Laptop()
+Lenovo=Laptop()
+
+Hp.ram="16Gb"
+Hp.processor="intel core ultra5"
+Hp.price=72000
+
+print(Hp.ram)
+print(Hp.processor)
+print(Hp.price)
